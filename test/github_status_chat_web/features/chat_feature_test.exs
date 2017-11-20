c45a231a7186a93bb0de64f9eaeabb1c9a5167ac
Alex Afshar <contact@alexafshar.com>
2017-11-20 00:52:21 -0500

    Created test for chat message

--- test/github_status_chat_web/features/chat_feature_test.exs
+++ test/github_status_chat_web/features/chat_feature_test.exs
@@ -1,7 +1,7 @@
 defmodule GithubStatusChatWeb.ChatFeatureTest do
   use GithubStatusChatWeb.FeatureCase, async: true
 
-  import Wallaby.Query, only: [css: 2, css: 1]
+  import Wallaby.Query, only: [css: 2, css: 1, text_field: 1]
 
   test "chat page has header for chat", %{session: session} do
     session
@@ -13,5 +13,12 @@ defmodule GithubStatusChatWeb.ChatFeatureTest do
     session
     |> visit("/")
     |> assert_has(css("#chat-input"))
+  end
+
+  test "user input is displayed", %{session: session} do
+    session
+    |> visit("/")
+    |> fill_in(text_field("chat-input"), with: "Hello")
+    |> assert_has(css(".message", text: "Hello"))
   end
 end
