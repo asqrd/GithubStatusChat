407be0fab9fbc99a8ead013af7e45c5cbd61ffb3
Alex Afshar <contact@alexafshar.com>
2017-11-20 00:43:08 -0500

    Added input for chat

--- test/github_status_chat_web/features/chat_feature_test.exs
+++ test/github_status_chat_web/features/chat_feature_test.exs
@@ -1,11 +1,17 @@
 defmodule GithubStatusChatWeb.ChatFeatureTest do
   use GithubStatusChatWeb.FeatureCase, async: true
 
-  import Wallaby.Query, only: [css: 2]
+  import Wallaby.Query, only: [css: 2, css: 1]
 
   test "chat page has header for chat", %{session: session} do
     session
     |> visit("/")
     |> assert_has(css(".chat-header", text: "Welcome to Chat"))
+  end
+
+  test "user can enter input", %{session: session} do
+    session
+    |> visit("/")
+    |> assert_has(css("#chat-input"))
   end
 end
